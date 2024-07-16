764e346922016b7c0cd625f447cd89c3eaa71650
NuclMe <saltansergei@gmail.com>
2024-07-16 22:27:39 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+- HTML,
+- SCSS,
+- Gulp,
+- JS
+```
+npm i
+```
+
+development mode:
+```
+gulp
+```
+
+version for publication:
+```
+gulp docs
+```
+
---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import { Fancybox } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
+
+Fancybox.bind('[data-fancybox]', {
+  // Your custom options
+});
+
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
+
